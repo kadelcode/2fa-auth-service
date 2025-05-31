eed6b1baaa3d7c54aa0b03604e6d824b1aab5cc1
kadelcode <kadelcode@gmail.com>
2025-05-31 15:53:39 +0100

    sendEmail utility function

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,18 @@
+const nodemailer = require('nodemailer');
+
+module.exports = async function sendEmail({ to, subject, html}) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        },
+    });
+
+    await transporter.sendMail({
+        from: `"2FA Auth App" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        html,
+    });
+};
