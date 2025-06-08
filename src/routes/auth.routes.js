5b992a6bc61242f7a441319170d71d3bad728891
kadelcode <kadelcode@gmail.com>
2025-06-08 13:41:42 +0100

    Social routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,9 @@
 // Import the Express framework
 const express = require('express');
 
+// Import passport.js 
+const passport = require('passport');
+
 // Create a router object to handle routes
 const router = express.Router();
 
@@ -24,6 +27,29 @@ router.post('/refresh-token', authController.refreshToken);
 
 // Define a POST route for requesting a password reset
 router.post('/forgot-password', authController.forgotPassword)
+
+// Google login
+router.get('/google', passport.authenticate('google', {scope: ['email', 'profile'] }));
+
+router.get(
+    '/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        // Optionally check user.is2FAEnabled
+        res.redirect('/dashboard')
+    }
+)
+
+// GitHub login
+router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get(
+    '/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    (req, res) => {
+        res.redirect('/dashboard')
+    }
+);
 
 // Export the router to make it available for use in other parts of the application
 module.exports = router;
