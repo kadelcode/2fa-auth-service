f1c35b53b628ba6ec3d99d515611ae3d68a09730
kadelcode <kadelcode@gmail.com>
2025-06-25 17:45:21 +0100

    Update code logic to verify social authentication

--- src/config/passport.js
+++ src/config/passport.js
@@ -22,12 +22,27 @@ passport.use(
                 let user = await User.findOne({ googleId: profile.id });
 
                 if (!user) {
+                    // If not found, try by email (in case user signed up locally)
+                    user = await User.findOne({ email: profile.emails[0].value });
                     // If user doesn't exist, create a new user in database
-                    user = await User.create({
+                    /*user = await User.create({
                         email: profile.emails[0].value, // Get email from Google profile
                         googleId: profile.id, // Google's unique ID for the user
                         name: profile.displayName, // User's display name from Google
-                    });
+                    });*/
+
+                    if (user) {
+                        // Link the existing account to Google
+                        user.googleId = profile.id;
+                        await user.save();
+                    } else {
+                        // No user found - create new one
+                        user = await User.create({
+                            email: profile.emails[0].value,
+                            googleId: profile.id,
+                            name: profile.displayName,
+                        })
+                    }
                 }
                 // Pass the user object to Passport
                 done(null, user);
@@ -54,12 +69,27 @@ passport.use(
                 let user = await User.findOne({ githubId: profile.id });
 
                 if (!user) {
+                    // If not found, try by email
+                    user = await User.findOne({ email: profile.emails[0].value });
+
+                    if (user) {
+                        // Link the existing account to GitHub
+                        user.githubId = profile.id
+                        await user.save();
+                    } else {
+                        // No user found - create a new one
+                        user = await User.create({
+                            email: profile.emails[0].value,
+                            githubId: profile.id,
+                            name: profile.displayName,
+                        })
+                    }
                     // If user doesn't exist, create a new user in database
-                    user = await User.create({
+                    /*user = await User.create({
                         email: profile.emails[0].value, // Get email from GitHub profile
                         githubId: profile.id, // GitHub's unique ID for the user
                         name: profile.displayName, // User's display name from GitHub
-                    });
+                    });*/
                 }
                 // Pass the user object to Passport
                 done(null, user);
